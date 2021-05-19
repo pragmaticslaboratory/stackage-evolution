36f350d1d5fa53cea3c3a95199b78a7dd01b63e5
Felipe Ruiz <felipe.ruiz@vacasa.com>
2021-05-18 20:14:43 -0400

    feat: correction to fix_path pipe

--- src/features/P5_fix_paths.py
+++ src/features/P5_fix_paths.py
@@ -1,36 +1,16 @@
-import argparse, logging, sys, copy, os
+import argparse
 import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("df", help="Dataframe file with package imports")
 parser.add_argument("pkgs_path", help="Directory of packages")
-parser.add_argument(
-    "-v",
-    "--verbose",
-    help="Set log level to DEBUG to increase output verbosity",
-    action="store_true",
-)
-parser.add_argument(
-    "-q",
-    "--quiet",
-    help="Set log level to ERROR to decrease output verbosity",
-    action="store_true",
-)
 
 args = parser.parse_args()
-if args.verbose:
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-elif args.quiet:
-    logging.basicConfig(level=logging.ERROR, stream=sys.stdout)
-else:
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
 df = pd.read_pickle(args.df)
 pkgs_path = args.pkgs_path
 
 for index, pkg in df.iterrows():
     provided_modules_found = pkg["provided-modules-found"]
-    
     new_list = []
     for (module, path) in provided_modules_found: 
         path = path.replace("//", "/")
@@ -38,5 +18,16 @@ for index, pkg in df.iterrows():
         new_list.append((module, path))
         
     df.at[index, "provided-modules-found"] = new_list
+
+    main_modules_found = pkg["main-modules-found"]
+    new_list = []
+    for path in main_modules_found: 
+        path = path.replace("./StackageDownload", pkgs_path)
+        new_list.append(path)
+        
+    df.at[index, "main-modules-found"] = new_list
+
+    cabal_file = pkg["cabal-file"]
+    df.at[index, "cabal-file"] = cabal_file.replace("/Users/fruiz/Desktop/github/papers-stackage/pipelines/scripts/StackageDownload", pkgs_path)
 
 df.to_pickle(args.df)
