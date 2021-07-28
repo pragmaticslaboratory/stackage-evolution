45087de6c3d9af43002a25acade6ef78d73b1c50
Ismael Figueroa <ifigueroap@gmail.com>
2021-07-28 11:50:40 -0400

    Fix formatting

--- src/parse/tests/test_parser.py
+++ src/parse/tests/test_parser.py
@@ -1,55 +1,63 @@
-import subprocess
-import os
 import json
+import os
 import re
+import subprocess
+
 
 def run_parser(case_path):
-    path_list = [os.path.join(os.path.dirname(__file__), 'inputs/test.cabal'), case_path]
+    path_list = [
+        os.path.join(os.path.dirname(__file__), "inputs/test.cabal"),
+        case_path,
+    ]
 
     complated_process = subprocess.run(
-        os.path.join(os.path.dirname(__file__), '../PackageInfoJSON'),
+        os.path.join(os.path.dirname(__file__), "../PackageInfoJSON"),
         stdout=subprocess.PIPE,
         input="\n".join(path_list),
-        text=True
+        text=True,
     )
 
-    result = re.findall(r'(?=\{).+?(?<=\})', complated_process.stdout)
-    return json.loads(result[0])['calls']
+    result = re.findall(r"(?=\{).+?(?<=\})", complated_process.stdout)
+    return json.loads(result[0])["calls"]
 
 
 def test_case1():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case1.txt")
-    calls = run_parser(filename)
+    calls = run_parser(filename)    
 
-    assert 'modify' in calls
+    assert "modify" in calls
+
 
 def test_case2():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case2.txt")
-    calls = run_parser(filename)
+    calls = run_parser(filename)    
 
-    assert 'runState' in calls
+    assert "runState" in calls
+
 
 def test_case3_1():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case3.txt")
-    calls = run_parser(filename)
+    calls = run_parser(filename)    
 
-    assert 'runState' in calls
+    assert "runState" in calls
+
 
 def test_case3_2():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case3.txt")
-    calls = run_parser(filename)
+    calls = run_parser(filename)    
 
-    assert 'execStateT' in calls
+    assert "execStateT" in calls
+
 
 def test_case4():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case4.txt")
-    calls = run_parser(filename)
+    calls = run_parser(filename)    
 
-    assert 'callCC' in calls
-    
+    assert "callCC" in calls
+
+
 def test_case5():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case5.txt")
-    calls = run_parser(filename)
+    calls = run_parser(filename)    
 
-    assert 'runListT' in calls
-    
+    assert "runListT" in calls
