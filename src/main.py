2c3aa25f6d83dcab33ea57e00bd9823ad9843029
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2021-09-21 16:25:39 -0300

    main with P2

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from util.logging import setup_log_level
 from util.parser import setup_command_line
 from features.P0_create_package_catalog import create_package_catalog
 from features.P1_construct_initial_dataframe import process_catalog_csv
+from features.P2_construct_dataframe_with_paths import construct_df_with_paths
 
 
 PATH = "C:/Users/nicol/Desktop/lts/lts-test"  # directory of packages
@@ -16,3 +17,5 @@ date_now = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
 csv_file = create_package_catalog(PATH, date_now, logging)
 initial_df = process_catalog_csv(os.path.join(
     os.path.dirname(__file__), csv_file), logging)
+
+df_with_paths = construct_df_with_paths(PATH, initial_df, logging)
