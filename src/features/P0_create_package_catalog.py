2fd9471b1fbbd6570e220f29999c61d26150b8d8
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2021-10-05 15:29:46 -0300

    Feature/automate pipelines and some refactoring
    * automating p0-p1
    
    * fixed P0 and P1
    
    * P2 working with some changes in P1
    
    * main with P2
    
    * Revert "P2 working with some changes in P1"
    
    This reverts commit 2f23d8759e251d7da3046cde1851e47591ddbd86.
    
    * Fixed P2
    
    * P3 and P4 implemented into main
    
    * Pipes ready to test with lts
    
    * change paths in P3,P6 and some changes in P5
    
    * Weird modules bug Fixed Pipe 2
    
    * automate create directorys, and fix some bugs
    
    * test
    
    * fix problem with files with text not in utf-8
    
    * automatized paths
    
    Co-authored-by: Felipe Ruiz <felipe.ruiz@vacasa.com>

--- src/features/P0_create_package_catalog.py
+++ src/features/P0_create_package_catalog.py
@@ -1,20 +1,21 @@
 import os
+import io
 import subprocess
-import csv
 import copy
 from datetime import datetime
-from lib.logging import setup_log_level
-from lib.parser import setup_command_line
 
 
-def create_csv(data, logging):
+def create_csv(data, date_now, logging, lts):
     logging.info("Writing results to file")
     try:
-        date_now = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-        csv_file_name = "packages-catalog-{date}.csv".format(date=date_now)
+        csv_path = os.path.join(os.path.dirname(__file__),
+                                "../../data/test/%s/packages-catalog-{date}.csv".format(
+            date=date_now) % lts)
+        csv_file_name = csv_path
         with open(csv_file_name, "w") as csv_file:
             csv_file.write("\n".join(data))
         logging.info("CSV file created")
+        return csv_file_name
     except IOError:
         logging.exception("Error: There was an error creating the file")
 
@@ -30,18 +31,26 @@ def walklevel(some_dir, level=1):
             del dirs[:]
 
 
-def run():
-    parser = setup_command_line()
-    args = parser.parse_args()
-    logging = setup_log_level(args)
+def clean_utf8(path_file):
+    file = io.open(path_file, 'r', encoding="utf-8")
 
+    test = file.read()
+    text = test.encode("ascii", "replace")
+    text = text.decode()
+    file.close()
+
+    file = io.open(path_file, 'w', encoding="utf-8")
+    file.write(text)
+    file.close()
+
+
+def create_package_catalog(path, date_now, logging, lts):
     _tmp_pkg_tuple_dirs = []
-    logging.info("Processing package index with root {path}".format(path=args.index))
-    for _, pkg_tuple, _ in walklevel(args.index, level=0):
+    logging.info("Processing package index with root {path}".format(path=path))
+    for _, pkg_tuple, _ in walklevel(path, level=0):
         _tmp_pkg_tuple_dirs.extend(pkg_tuple)
-        _tmp_pkg_tuple_dirs = map(
-            lambda x: (x, os.path.join(args.index, x)), _tmp_pkg_tuple_dirs
-        )
+        _tmp_pkg_tuple_dirs = map(lambda x: (
+            x, os.path.join(path, x)), _tmp_pkg_tuple_dirs)
 
     pkg_dirs = []
     for pkg_tuple in _tmp_pkg_tuple_dirs:
@@ -49,19 +58,22 @@ def run():
         for _, versions, _ in walklevel(pkg_path, level=0):
             new_tuple = copy.deepcopy(pkg_tuple + (versions,))
             pkg_dirs.append(new_tuple)
-
+        logging.debug(pkg_tuple)
     total_pkgs = len(list(pkg_dirs))
-    logging.info("Total Packages for Analysis: {total}".format(total=total_pkgs))
+    logging.info(
+        "Total Packages for Analysis: {total}".format(total=total_pkgs))
     logging.info("Starting work at %s" % str(datetime.now()))
 
     package_catalog = list()
     for pkg in pkg_dirs:
         (name, pkg_path, name_version) = pkg
         cabal_file = os.path.join(pkg_path, name_version[0], name + ".cabal")
-        logging.info("Starting work at {cabal_file}".format(cabal_file=cabal_file))
-
+        logging.info("Starting work at {cabal_file}".format(
+            cabal_file=cabal_file))
+        clean_utf8(cabal_file)
         completed_process = subprocess.run(
-            "/Users/fruiz/.local/bin/parse-exe",
+            os.path.join(os.path.dirname(__file__),
+                         '../parse/ParseCabal.exe'),
             input=cabal_file,
             capture_output=True,
             text=True,
@@ -71,4 +83,4 @@ def run():
         if completed_process.returncode == 0:
             package_catalog.append(completed_process.stdout)
 
-    create_csv(package_catalog, logging)
+    return create_csv(package_catalog, date_now, logging, lts)
--- src/util/parser.py
+++ src/util/parser.py
@@ -5,8 +5,6 @@ def setup_command_line():
     parser = argparse.ArgumentParser(
         description='Generate a CSV file with package catalog')
     parser.add_argument(
-        "index", help="Path to directory holding the uncompressed Hackage index")
-    parser.add_argument(
         "-v", "--verbose", help="Set log level to DEBUG to increase output verbosity", action="store_true")
     parser.add_argument(
         "-q", "--quiet", help="Set log level to ERROR to decrease output verbosity", action="store_true")
