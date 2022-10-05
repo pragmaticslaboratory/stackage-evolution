9e68e9d5ed6079bcc165b3bfbc0408e39db10f06
Ismael Figueroa <ifigueroap@gmail.com>
2022-10-04 23:07:43 -0300

    Some configuration work to more easily run the project tasks.

--- src/parse/tests/package-info-json/test_package_info.py
+++ src/parse/tests/package-info-json/test_package_info.py
@@ -23,62 +23,62 @@ def run_parser(case_path):
 
 def test_case1():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case1.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "modify" in calls
 
 
 def test_case2():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case2.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "runState" in calls
 
 
 def test_case3_1():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case3.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "runState" in calls
 
 
 def test_case3_2():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case3.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "execStateT" in calls
 
 
 def test_case4():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case4.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "callCC" in calls
 
 
 def test_case5():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case5.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "runListT" in calls
 
 
 def test_case6():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case6.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "evalStateT" in calls
 
 
 def test_case7():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case7.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "evalState" in calls
 
 
 def test_case8():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case8.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
     assert "evalState" in calls
