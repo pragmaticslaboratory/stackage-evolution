fa67eecf8afbf260bc72808c1d5eb4efddc17e19
Ismael Figueroa <ifigueroap@gmail.com>
2022-10-11 18:18:50 -0300

    Fix 'flip case' and 'App Parens' case, thus fixing tests cases 6, 7, and 8.

--- src/parse/tests/package-info-json/test_package_info.py
+++ src/parse/tests/package-info-json/test_package_info.py
@@ -3,6 +3,39 @@ import os
 import re
 import subprocess
 
+x = """
+tests/package-info-json/test_package_info.py::test_case1 PASSED
+tests/package-info-json/test_package_info.py::test_case2 PASSED
+tests/package-info-json/test_package_info.py::test_case3_1 PASSED
+tests/package-info-json/test_package_info.py::test_case3_2 PASSED
+tests/package-info-json/test_package_info.py::test_case4 PASSED
+tests/package-info-json/test_package_info.py::test_case5 PASSED
+tests/package-info-json/test_package_info.py::test_case6 FAILED -> PASSED (Parens App case)
+tests/package-info-json/test_package_info.py::test_case7 FAILED -> PASSED (flip case)
+tests/package-info-json/test_package_info.py::test_case8 FAILED -> PASSED (flip case)
+tests/package-info-json/test_package_info.py::test_case9 FAILED
+tests/package-info-json/test_package_info.py::test_case10 FAILED
+tests/package-info-json/test_package_info.py::test_case11 FAILED
+tests/package-info-json/test_package_info.py::test_case12 FAILED
+tests/package-info-json/test_package_info.py::test_case13 FAILED
+tests/package-info-json/test_package_info.py::test_case14 FAILED
+tests/package-info-json/test_package_info.py::test_case15 FAILED
+tests/package-info-json/test_package_info.py::test_case16 FAILED
+tests/package-info-json/test_package_info.py::test_case17 FAILED
+tests/package-info-json/test_package_info.py::test_case18 FAILED
+tests/package-info-json/test_package_info.py::test_case19 FAILED
+tests/package-info-json/test_package_info.py::test_case20 FAILED
+tests/package-info-json/test_package_info.py::test_case21 FAILED
+tests/package-info-json/test_package_info.py::test_case22 FAILED
+tests/package-info-json/test_package_info.py::test_case23 FAILED
+tests/package-info-json/test_package_info.py::test_case24 FAILED
+tests/package-info-json/test_package_info.py::test_case25 FAILED
+tests/package-info-json/test_package_info.py::test_case26 FAILED
+tests/package-info-json/test_package_info.py::test_case27 FAILED
+tests/parse-cabal/test_parse_cabal.py::test_bytestring PASSED
+tests/parse-cabal/test_parse_cabal.py::test_aws PASSED
+"""
+
 
 def run_parser(case_path):
     path_list = [
@@ -31,6 +64,7 @@ def test_case1():
 def test_case2():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case2.txt")
     calls = run_parser(filename)
+    print(calls)
 
     assert "runState" in calls
 
@@ -74,125 +108,194 @@ def test_case7():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case7.txt")
     calls = run_parser(filename)
 
-    assert "evalState" in calls
+    # evalState is not in the source code of case7.txt
+    # TODO: what is the proper test case here?
+    # assert "evalState" in calls
+
+    # I changed 'evalStateT' to 'execState' to make sense in the source code
+    assert "execState" in calls
 
 
 def test_case8():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case8.txt")
     calls = run_parser(filename)
 
+    # In this expression: flip evalState state
+    # evalState is used as argument parameter, but it is not called as a function.
+    # I mean, it is called indirectly, through flip.
+
+    # We added a special case where the first variable-ident argument of flip
+    # is considered as a call
+
     assert "evalState" in calls
+
 
 def test_case9():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case9.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case10():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case10.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case11():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case11.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case12():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case12.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
-    
+
+
 def test_case13():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case13.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case14():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case14.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case15():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case15.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case16():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case16.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case17():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case17.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case18():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case18.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case19():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case19.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case20():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case20.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case21():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case21.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case22():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case22.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case23():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case23.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case24():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case24.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case25():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case25.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case26():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case26.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
+
 
 def test_case27():
     filename = os.path.join(os.path.dirname(__file__), "inputs/case27.txt")
-    calls = run_parser(filename)    
+    calls = run_parser(filename)
 
+    # evalState is not in the source code!
+    # TODO: what is the proper test case here?
     assert "evalState" in calls
