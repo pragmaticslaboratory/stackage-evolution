5850c9f7be57ff58354d078949cc777482952e78
Ismael Figueroa <ifigueroap@gmail.com>
2023-03-15 12:42:57 -0300

    Parse/fix cabal parser (#6)
    * Fix parse-cabal tests
    
    * add 6 new cases for the parser
    
    * Some configuration work to more easily run the project tasks.
    
    * Add justfile test command
    
    * new test cases
    
    * Fix 'flip case' and 'App Parens' case, thus fixing tests cases 6, 7, and 8.
    
    * fix: case for parser
    
    * Fixing parse test errors.
    
    ---------
    
    Co-authored-by: NicoSv19 <39229122+NicoSv19@users.noreply.github.com>

---
+++ src/parse/tests/package-info-json/test_package_info.py
@@ -0,0 +1,305 @@
+import json
+import os
+import re
+import subprocess
+
+x = """
+tests/package-info-json/test_package_info.py::test_case1 PASSED
+tests/package-info-json/test_package_info.py::test_case2 PASSED
+tests/package-info-json/test_package_info.py::test_case3_1 PASSED
+tests/package-info-json/test_package_info.py::test_case3_2 PASSED
+tests/package-info-json/test_package_info.py::test_case4 PASSED
+tests/package-info-json/test_package_info.py::test_case5 PASSED
+tests/package-info-json/test_package_info.py::test_case6 FAILED -> PASSED (Parens App case)
+tests/package-info-json/test_package_info.py::test_case7 FAILED -> PASSED (flip case)
+tests/package-info-json/test_package_info.py::test_case8 FAILED -> PASSED (flip case)
+tests/package-info-json/test_package_info.py::test_case9 FAILED
+tests/package-info-json/test_package_info.py::test_case10 FAILED
+tests/package-info-json/test_package_info.py::test_case11 FAILED
+tests/package-info-json/test_package_info.py::test_case12 FAILED
+tests/package-info-json/test_package_info.py::test_case13 FAILED
+tests/package-info-json/test_package_info.py::test_case14 FAILED
+tests/package-info-json/test_package_info.py::test_case15 FAILED
+tests/package-info-json/test_package_info.py::test_case16 FAILED
+tests/package-info-json/test_package_info.py::test_case17 FAILED
+tests/package-info-json/test_package_info.py::test_case18 FAILED
+tests/package-info-json/test_package_info.py::test_case19 FAILED
+tests/package-info-json/test_package_info.py::test_case20 FAILED
+tests/package-info-json/test_package_info.py::test_case21 FAILED
+tests/package-info-json/test_package_info.py::test_case22 FAILED
+tests/package-info-json/test_package_info.py::test_case23 FAILED
+tests/package-info-json/test_package_info.py::test_case24 FAILED
+tests/package-info-json/test_package_info.py::test_case25 FAILED
+tests/package-info-json/test_package_info.py::test_case26 FAILED
+tests/package-info-json/test_package_info.py::test_case27 FAILED
+tests/parse-cabal/test_parse_cabal.py::test_bytestring PASSED
+tests/parse-cabal/test_parse_cabal.py::test_aws PASSED
+"""
+
+x2 = """
+tests/package-info-json/test_package_info.py::test_case1 PASSED
+tests/package-info-json/test_package_info.py::test_case2 PASSED
+tests/package-info-json/test_package_info.py::test_case3_1 PASSED
+tests/package-info-json/test_package_info.py::test_case3_2 PASSED
+tests/package-info-json/test_package_info.py::test_case4 PASSED
+tests/package-info-json/test_package_info.py::test_case5 PASSED
+tests/package-info-json/test_package_info.py::test_case6 PASSED
+tests/package-info-json/test_package_info.py::test_case7 PASSED
+tests/package-info-json/test_package_info.py::test_case8 PASSED
+tests/package-info-json/test_package_info.py::test_case9 FAILED***
+tests/package-info-json/test_package_info.py::test_case10 PASSED
+tests/package-info-json/test_package_info.py::test_case11 FAILED***
+tests/package-info-json/test_package_info.py::test_case12 PASSED
+tests/package-info-json/test_package_info.py::test_case13 FAILED***
+tests/package-info-json/test_package_info.py::test_case14 FAILED**
+tests/package-info-json/test_package_info.py::test_case15 PASSED
+tests/package-info-json/test_package_info.py::test_case16 PASSED
+tests/package-info-json/test_package_info.py::test_case17 FAILED***
+tests/package-info-json/test_package_info.py::test_case18 PASSED
+tests/package-info-json/test_package_info.py::test_case19 PASSED
+tests/package-info-json/test_package_info.py::test_case20 FAILED***
+tests/package-info-json/test_package_info.py::test_case21 FAILED***
+tests/package-info-json/test_package_info.py::test_case22 PASSED
+tests/package-info-json/test_package_info.py::test_case23 FAILED***
+tests/package-info-json/test_package_info.py::test_case24 PASSED
+tests/package-info-json/test_package_info.py::test_case25 FAILED***
+tests/package-info-json/test_package_info.py::test_case26 PASSED
+tests/package-info-json/test_package_info.py::test_case27 FAILED***
+tests/parse-cabal/test_parse_cabal.py::test_bytestring PASSED
+tests/parse-cabal/test_parse_cabal.py::test_aws PASSED
+"""
+
+
+def run_parser(case_path):
+    path_list = [
+        os.path.join(os.path.dirname(__file__), "inputs/test.cabal"),
+        case_path,
+    ]
+
+    complated_process = subprocess.run(
+        os.path.join(os.path.dirname(__file__), "../../PackageInfoJSON"),
+        stdout=subprocess.PIPE,
+        input="\n".join(path_list),
+        text=True,
+    )
+
+    result = re.findall(r"(?=\{).+?(?<=\})", complated_process.stdout)
+    return json.loads(result[0])["calls"]
+
+
+def test_case1():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case1.txt")
+    calls = run_parser(filename)
+
+    assert "modify" in calls
+
+
+def test_case2():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case2.txt")
+    calls = run_parser(filename)
+    print(calls)
+
+    assert "runState" in calls
+
+
+def test_case3_1():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case3.txt")
+    calls = run_parser(filename)
+
+    assert "runState" in calls
+
+
+def test_case3_2():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case3.txt")
+    calls = run_parser(filename)
+
+    assert "execStateT" in calls
+
+
+def test_case4():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case4.txt")
+    calls = run_parser(filename)
+
+    assert "callCC" in calls
+
+
+def test_case5():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case5.txt")
+    calls = run_parser(filename)
+
+    assert "runListT" in calls
+
+
+def test_case6():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case6.txt")
+    calls = run_parser(filename)
+
+    assert "evalStateT" in calls
+
+
+def test_case7():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case7.txt")
+    calls = run_parser(filename)
+
+    # evalState is not in the source code of case7.txt
+    # TODO: what is the proper test case here?
+    # assert "evalState" in calls
+
+    # I changed 'evalStateT' to 'execState' to make sense in the source code
+    assert "execState" in calls
+
+
+def test_case8():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case8.txt")
+    calls = run_parser(filename)
+
+    # In this expression: flip evalState state
+    # evalState is used as argument parameter, but it is not called as a function.
+    # I mean, it is called indirectly, through flip.
+
+    # We added a special case where the first variable-ident argument of flip
+    # is considered as a call
+
+    assert "evalState" in calls
+
+
+def test_case9():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case9.txt")
+    calls = run_parser(filename)
+
+    # This case seems incorrect, as MaybeT appears in the *type signature*
+    # but is not called in the code. In fact, MaybeT does not exists as data constructor:
+    # its variants are constructed using either Just or Nothing, and those cases are
+    # already appearing in the calls data.
+    assert "MaybeT" in calls
+
+
+def test_case10():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case10.txt")
+    calls = run_parser(filename)
+
+    assert "runReaderT" in calls
+
+
+def test_case11():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case11.txt")
+    calls = run_parser(filename)
+
+    assert "throwError" in calls
+    # assert "runTCMTop" in call
+    # assert "null" in calls
+    # assert "liftIO" in calls
+    # assert "putStr" in calls
+    # assert "unlines" in calls
+
+
+def test_case12():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case12.txt")
+    calls = run_parser(filename)
+
+    assert "liftM" in calls
+
+
+def test_case13():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case13.txt")
+    calls = run_parser(filename)
+
+    assert "rwsP" in calls
+
+
+def test_case14():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case14.txt")
+    calls = run_parser(filename)
+
+    assert "execRWSP and fmap" in calls
+
+
+def test_case15():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case15.txt")
+    calls = run_parser(filename)
+
+    assert "withReaderT" in calls
+
+
+def test_case16():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case16.txt")
+    calls = run_parser(filename)
+
+    assert "modify" in calls
+
+
+def test_case17():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case17.txt")
+    calls = run_parser(filename)
+
+    assert "runStateT" in calls
+
+
+def test_case18():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case18.txt")
+    calls = run_parser(filename)
+
+    assert "evalStateT" in calls
+
+
+def test_case19():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case19.txt")
+    calls = run_parser(filename)
+
+    assert "lift" in calls
+
+
+def test_case20():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case20.txt")
+    calls = run_parser(filename)
+
+    assert "StateT" in calls
+
+
+def test_case21():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case21.txt")
+    calls = run_parser(filename)
+
+    assert "evalStateT" in calls
+
+
+def test_case22():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case22.txt")
+    calls = run_parser(filename)
+
+    assert "runWriterT" in calls
+
+
+def test_case23():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case23.txt")
+    calls = run_parser(filename)
+
+    assert "mapExceptT" in calls
+
+
+def test_case24():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case24.txt")
+    calls = run_parser(filename)
+
+    assert "mapAccumL" in calls
+
+
+def test_case25():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case25.txt")
+    calls = run_parser(filename)
+
+    assert "liftCallCC" in calls
+
+
+def test_case26():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case26.txt")
+    calls = run_parser(filename)
+
+    assert "liftF" in calls
+
+
+def test_case27():
+    filename = os.path.join(os.path.dirname(__file__), "inputs/case27.txt")
+    calls = run_parser(filename)
+
+    assert "unfoldr" in calls
